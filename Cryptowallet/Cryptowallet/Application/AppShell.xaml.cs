7ce2cc870633b13fc64119e3bc4dd9a09e329034
Garnett05 <charlleslimamota10@hotmail.com>
2020-11-22 17:23:02 -0300

    - Creating the menu and bar inside a menu item;

---
+++ Cryptowallet/Cryptowallet/Application/AppShell.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Cryptowallet.Application
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            BindingContext = new AppShellViewModel();
+        }
+    }
+}
---
+++ Cryptowallet/Cryptowallet/Application/AppShellViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Cryptowallet.Application
+{
+    public class AppShellViewModel
+    {
+        public ICommand SignOutCommand { get => new Command(async () => await SignOut()); }
+
+        private async Task SignOut()
+        {
+            await Shell.Current.DisplayAlert("Todo", "You have been logged out", "Ok");
+        }
+    }
+}
