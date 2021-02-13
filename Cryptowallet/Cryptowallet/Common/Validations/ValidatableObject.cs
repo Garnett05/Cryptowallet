b15d533386fe6a3c798fdd966a71a64818466eef
Charlles Lima <charlleslimamota10@hotmail.com>
2021-02-13 18:56:58 -0300

    - Adding sqlite-net-pcl package; - Creating the logic to validate the entries by the user; - Created a database structure to save all the data (all files in Database and Extensions folders);

---
+++ Cryptowallet/Cryptowallet/Common/Validations/ValidatableObject.cs
@@ -0,0 +1,50 @@
+﻿using Cryptowallet.Common.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cryptowallet.Common.Validations
+{
+    public class ValidatableObject<T> : ExtendedBindableObject
+    {
+        public List<IValidationRule<T>> Validations { get; }
+
+        private List<string> _errors;
+        public List<string> Errors
+        {
+            get => _errors;
+            set { SetProperty(ref _errors, value); }
+        }
+        private T _value;
+        public T Value
+        {
+            get => _value;
+            set { SetProperty(ref _value, value); }
+        }
+        private bool _isValid;
+        public bool IsValid
+        {
+            get => _isValid;
+            set { SetProperty(ref _isValid, value); }
+        }
+
+        public ValidatableObject()
+        {
+            _isValid = true;
+            _errors = new List<string>();
+            Validations = new List<IValidationRule<T>>();
+        }
+        public bool Validate()
+        {
+            Errors.Clear();
+
+            IEnumerable<string> errors = Validations.Where(x => !x.Check(Value)).Select(x => x.ValidationMessage);
+
+            Errors = errors.ToList();
+            IsValid = !Errors.Any();
+
+            return IsValid;
+        }
+    }
+}
--- Cryptowallet/Cryptowallet/Modules/AddTransaction/AddTransactionViewModel.cs
+++ Cryptowallet/Cryptowallet/Modules/AddTransaction/AddTransactionViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Cryptowallet.Common.Base;
 using Cryptowallet.Common.Models;
+using Cryptowallet.Common.Validations;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -14,21 +15,12 @@ namespace Cryptowallet.Modules.AddTransaction
     public class AddTransactionViewModel : BaseViewModel
     {
         private bool _isDeposit;
-        private string _id;
-        private ObservableCollection<Coin> _availableAssets;
-        private Coin _selectedCoin;
-        private DateTime _transactionDate;
-
-        public AddTransactionViewModel()
-        {
-            AvailableAssets = new ObservableCollection<Coin>(Coin.GetAvailableAssets());
-        }
-
         public bool IsDeposit
         {
             get => _isDeposit;
             set { SetProperty(ref _isDeposit, value); }
         }
+        private string _id;
         public string Id
         {
             set
@@ -36,26 +28,45 @@ namespace Cryptowallet.Modules.AddTransaction
                 _id = Uri.UnescapeDataString(value);
             }
         }
+        private ObservableCollection<Coin> _availableAssets;
         public ObservableCollection<Coin> AvailableAssets
         {
             get => _availableAssets;
             set { SetProperty(ref _availableAssets, value); }
         }
+        private ValidatableObject<decimal> _amount;
+        public ValidatableObject<decimal> Amount
+        {
+            get => _amount;
+            set { SetProperty(ref _amount, value); }
+        }
+        private Coin _selectedCoin;
         public Coin SelectedCoin
         {
             get => _selectedCoin;
             set { SetProperty(ref _selectedCoin, value); }
         }
+        private DateTime _transactionDate;
         public DateTime TransactionDate
         {
             get => _transactionDate;
             set { SetProperty(ref _transactionDate, value); }
         }
+        public AddTransactionViewModel()
+        {
+            AvailableAssets = new ObservableCollection<Coin>(Coin.GetAvailableAssets());
+            _amount = new ValidatableObject<decimal>();
+            _amount.Validations.Add(new NonNegativeRule { ValidationMessage = "Please enter amount greater than zero." });
+        }        
         public ICommand AddTransactionCommand { get => new Command(async () => await AddTransaction()); }
 
         private async Task AddTransaction()
         {
-            //throw new NotImplementedException();
+            _amount.Validate();
+            if (!_amount.IsValid)
+            {
+                return;
+            }
         }
     }
 }
